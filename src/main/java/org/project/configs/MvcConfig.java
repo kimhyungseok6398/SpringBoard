b3ce1cd64a62cb82569aefcdd5491ade42c68298
kimhyungseok6398 <gudtjr6398@naver.com>
2023-10-13 11:01:49 +0900

    스프링 시큐리티 기본설정 , 정적경로 설정 , 임시 페이지 추가

--- src/main/java/org/project/configs/MvcConfig.java
+++ src/main/java/org/project/configs/MvcConfig.java
@@ -9,11 +9,14 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//설정의 틀
+//설정의 틀 스프링 부트시 필요한것만 사용함
+// 인터페이스 설정
+// 오버라이드 설정
 @Configuration
 @RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
-
+    // 스프링 빈으로 등록된 객체에서 properties파일이나
+    // 환경변수등의 값을 주입받을때 사용
     @Value("${file.upload.path}")
     private String fileUploadPath;
 
@@ -26,16 +29,20 @@ public class MvcConfig implements WebMvcConfigurer {
         WebMvcConfigurer.super.addViewControllers(registry);
     registry.addViewController("/")
             .setViewName("main/index");
+            // 템플릿을 만들기 위해 임시로 설정
             // 임시로 설정 바뀔수 있음
             // 인터페이스 설정 잘 기억하기
+            // 레이아웃 설정후 삭제예정
     }
 
-
+    // 정적경로들을 주로 설정하기 위해 사용
+    // 파일들의 URL등을 설정
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         WebMvcConfigurer.super.addResourceHandlers(registry);
     registry.addResourceHandler("/uploads/**")
             .addResourceLocations("file:///"+fileUploadPath);
+    // 경로 설정
     }
 
     @Override    // 기본설정도 볼수있게??
--- src/main/java/org/project/configs/SecurityConfig.java
+++ src/main/java/org/project/configs/SecurityConfig.java
@@ -8,10 +8,10 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@Configuration // 스프링에서 설정클래스로 인식
 public class SecurityConfig {
 // 북따옴엔 없음
-    @Bean
+    @Bean // 스프링이 관리하는 객체
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
     return http.build(); // 추후 설정 결정
     }
@@ -19,7 +19,8 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer(){
         return w->w.ignoring().requestMatchers("/css/**", "/js/**","/images/**","/error/**");
-
+        // 정적경로 설정 상관없이 접근 가능하게 설정
+        // 레이아웃 설정후 해제 예정
     }
 
 }
